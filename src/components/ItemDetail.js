0c2311d8ebb6140cf94461e25ea4e3743eac1fdb
Lucas Tabare <lucas.tabare@gmail.com>
2022-05-28 21:54:44 -0300

    cambios en itemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,6 +1,10 @@
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ item }) => {
+  const onAdd = (contador) => {
+    alert("Has agregado " + contador + "Items al carrito");
+  };
+
   return (
     <div>
       <h2>Producto: {item.name}</h2>
@@ -8,7 +12,7 @@ const ItemDetail = ({ item }) => {
       <p>Description: {item.description}</p>
       <p>Precio: {item.price}</p>
       <p>Stock: {item.stock}</p>
-      <ItemCount />
+      <ItemCount stock={item.stock} initial={1} onAdd={onAdd} />
     </div>
   );
 };
